84c908e4597e91d597410b2d898b1b55a7933af5
Gabriel Aparecido Tinelli Ferrarini <gabriel.ferrarini@outlook.com>
2017-03-11 18:51:57 -0300

    Lecture 63

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,7 @@
 #include "BuildingEscape.h"
 #include "Grabber.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -30,6 +31,21 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
-}
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
 
+	// Get the player view point this tick
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation);
+
+	// Log out to test
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s. Rotation: %s"),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString()
+	)
+
+	// Ray-cast out to reach distance
+
+	// See what we hit
+}
